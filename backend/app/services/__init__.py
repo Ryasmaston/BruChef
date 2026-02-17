c061736286fccdbd33f8862714048364b4b34b6e
ryan.osmaston <ryan.osmaston@gmail.com>
2026-02-17 19:04:26 +0000

    add auth services

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,3 +1,4 @@
 from .cocktail_service import CocktailService
 from .ingredient_service import IngredientService
-__all__ = ['CocktailService', 'IngredientService']
+from .auth_service import AuthService
+__all__ = ['CocktailService', 'IngredientService', 'AuthService']
